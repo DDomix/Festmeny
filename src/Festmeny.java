f91cc3c3a2898c5c3a938d549995fad4dd28b4c8
DDomix <papp.dominik@petrik.hu>
2022-09-22 09:31:43 +0200

    Fájlbeolvasás

--- src/Festmeny.java
+++ src/Festmeny.java
@@ -1,10 +1,35 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.time.LocalDateTime;
+
 
 public class Festmeny {
     String cim;
     String festo;
+    String stilus;
     int licitekszama = 0;
     int legmagasabblicit = 0;
     LocalDateTime legutolsolicitideje;
     boolean elkelt = false;
+
+    public Festmeny(String cim, String festo, String stilus) {
+        this.cim = cim;
+        this.festo = festo;
+        this.stilus = stilus;
+    }
+    public  Festmeny(String row){
+        var s=row.split(";");
+        this.cim = s[0];
+        this.festo = s[1];
+        this.stilus = s[2];
+    }
+
+    @Override
+    public String toString() {
+        return
+                "cim='" + cim + '\'' +
+                ", festo='" + festo + '\'' +
+                ", stilus='" + stilus + '\'' +
+                '}';
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.LinkedList;
+import java.util.List;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws IOException{
+        var lines= Files.readAllLines(Paths.get("festmenyek.csv"));
+        var paintings=new LinkedList<Festmeny>();
+        for (var line:lines){
+            var festmenyek=new Festmeny(line);
+            paintings.add(festmenyek);
+        }
+        System.out.println(paintings);
     }
 }
