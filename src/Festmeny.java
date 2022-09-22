6b78825752c18bb08d2e5bcf6b0303d3055098a9
DDomix <papp.dominik@petrik.hu>
2022-09-22 08:14:40 +0200

    initial commit

---
+++ src/Festmeny.java
@@ -0,0 +1,10 @@
+import java.time.LocalDateTime;
+
+public class Festmeny {
+    String cim;
+    String festo;
+    int licitekszama = 0;
+    int legmagasabblicit = 0;
+    LocalDateTime legutolsolicitideje;
+    boolean elkelt = false;
+}
